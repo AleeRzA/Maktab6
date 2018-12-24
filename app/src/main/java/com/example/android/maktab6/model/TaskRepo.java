26a6bd633cfc102e909f5468bd7caeab025b5a60
alirezanasr <alireza_nasrollahi@yahoo.com>
2018-12-24 23:23:47 +0330

    add addToList method..

--- app/src/main/java/com/example/android/maktab6/model/TaskRepo.java
+++ app/src/main/java/com/example/android/maktab6/model/TaskRepo.java
@@ -1,13 +1,21 @@
 package com.example.android.maktab6.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskRepo {
     private static TaskRepo instance;
+    private List<Task> mTasks;
     private TaskRepo(){
-
+        mTasks = new ArrayList<>();
     }
     public static TaskRepo getInstance(){
         if(instance == null)
             instance = new TaskRepo();
         return instance;
+    }
+    public List<Task> addToList(Task task){
+         mTasks.add(task);
+         return mTasks;
     }
 }
