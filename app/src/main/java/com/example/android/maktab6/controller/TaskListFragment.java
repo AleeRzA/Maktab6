797621fb04cf5089f8e08f0d1093b5568ebc9464
alirezanasr <alireza_nasrollahi@yahoo.com>
2018-12-24 12:46:55 +0330

    newInstance is created for the fragment...

--- app/src/main/java/com/example/android/maktab6/controller/TaskListFragment.java
+++ app/src/main/java/com/example/android/maktab6/controller/TaskListFragment.java
@@ -9,16 +9,28 @@ import android.view.ViewGroup;
 
 import com.example.android.maktab6.R;
 
+import java.util.UUID;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TaskListFragment extends Fragment {
 
 
+    public static final String ARGS_TAK_ID = "com.example.android.maktab6.controller.args_takId";
+
     public TaskListFragment() {
         // Required empty public constructor
     }
 
+    public static TaskListFragment newInstance(UUID taskId) {
+
+        Bundle args = new Bundle();
+        args.putSerializable(ARGS_TAK_ID, taskId);
+        TaskListFragment fragment = new TaskListFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
