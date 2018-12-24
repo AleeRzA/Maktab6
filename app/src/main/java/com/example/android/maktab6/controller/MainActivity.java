b9150aad4e35c01a3fdbdcc7d24b43064eff504c
alirezanasr <alireza_nasrollahi@yahoo.com>
2018-12-24 12:43:44 +0330

    Main Activity modifications...

--- app/src/main/java/com/example/android/maktab6/controller/MainActivity.java
+++ app/src/main/java/com/example/android/maktab6/controller/MainActivity.java
@@ -1,15 +1,49 @@
 package com.example.android.maktab6.controller;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.android.maktab6.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    private FloatingActionButton mActionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewPager = findViewById(R.id.main_view_pager);
+        mTabLayout = findViewById(R.id.main_tabLayout);
+        mActionButton = findViewById(R.id.main_floating_btn);
+
+        mTabLayout.setupWithViewPager(mViewPager);
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public int getCount() {
+                return 3;
+            }
+        });
+        mActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "hi", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
