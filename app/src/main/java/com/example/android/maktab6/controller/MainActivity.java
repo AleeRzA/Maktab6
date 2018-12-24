6e42858b6838e724b206c7f132881f32a87c80d7
alirezanasr <alireza_nasrollahi@yahoo.com>
2018-12-24 10:57:46 +0330

    controller package is created MainActivity moved into it...

--- app/src/main/java/com/example/android/maktab6/controller/MainActivity.java
+++ app/src/main/java/com/example/android/maktab6/controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.android.maktab6;
+package com.example.android.maktab6.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.android.maktab6.R;
 
 public class MainActivity extends AppCompatActivity {
 
