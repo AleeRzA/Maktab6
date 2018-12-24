f49de5df9c7ea710040ba2ddacaa409e18ff3843
alirezanasr <alireza_nasrollahi@yahoo.com>
2018-12-25 01:07:03 +0330

    Add fragment to the Activity...

--- app/src/main/java/com/example/android/maktab6/controller/TaskCreationActivity.java
+++ app/src/main/java/com/example/android/maktab6/controller/TaskCreationActivity.java
@@ -3,6 +3,7 @@ package com.example.android.maktab6.controller;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.android.maktab6.R;
@@ -17,5 +18,13 @@ public class TaskCreationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_creation);
+
+        TaskCreationFragment fragment = new TaskCreationFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        if(manager.findFragmentById(R.id.container_layout) == null){
+            manager.beginTransaction()
+                    .add(R.id.container_layout, fragment)
+                    .commit();
+        }
     }
 }
