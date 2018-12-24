26424db43fa6b23b38bbe95bc623f83cd4702116
alirezanasr <alireza_nasrollahi@yahoo.com>
2018-12-24 10:43:15 +0330

    Task class is created.. Comments added...

---
+++ app/src/main/java/com/example/android/maktab6/model/Task.java
@@ -0,0 +1,50 @@
+package com.example.android.maktab6.model;
+
+import android.content.Context;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Here is the Task class which has four fields
+ * Title, Description, Date and an unique Id...
+ */
+public class Task {
+    private String mTitle;
+    private String mDescription;
+    private Date mDate;
+    private UUID mId;
+
+    public Task(Context context){
+        mId = UUID.randomUUID();
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+}
