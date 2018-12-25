ccb5f87c2cdef46c34e12c5b53ceca05e10eaa23
alirezanasr <alireza_nasrollahi@yahoo.com>
2018-12-25 11:07:39 +0330

    TaskCreation button functionality.

--- app/src/main/java/com/example/android/maktab6/controller/TaskCreationFragment.java
+++ app/src/main/java/com/example/android/maktab6/controller/TaskCreationFragment.java
@@ -4,11 +4,14 @@ package com.example.android.maktab6.controller;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.android.maktab6.R;
 import com.example.android.maktab6.model.Task;
@@ -19,7 +22,7 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TaskCreationFragment extends Fragment {
+public class TaskCreationFragment extends Fragment  {
 
     private EditText mTitle;
     private EditText mDescription;
@@ -46,6 +49,47 @@ public class TaskCreationFragment extends Fragment {
         mDescription = view.findViewById(R.id.creation_description);
         mCreateBtn = view.findViewById(R.id.creation_btn_create);
 
+        mTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                newTask.setTitle(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        mDescription.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                newTask.setDescription(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTaskList.add(newTask);
+                Toast.makeText(getActivity(), R.string.task_saved_message, Toast.LENGTH_SHORT).show();
+                ((TaskCreationActivity)getActivity()).onBackPressed();
+            }
+        });
         return view;
     }
 
