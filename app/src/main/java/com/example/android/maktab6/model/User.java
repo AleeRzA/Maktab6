f72e0e3d38be2474791c0651f97f7d46c1c65837
alirezanasr <alireza_nasrollahi@yahoo.com>
2019-01-08 18:33:48 +0330

    Creating user model

---
+++ app/src/main/java/com/example/android/maktab6/model/User.java
@@ -0,0 +1,44 @@
+package com.example.android.maktab6.model;
+
+import java.util.UUID;
+
+public class User {
+    private String mName;
+    private String mUserName;
+    private String mPassword;
+    private UUID mId;
+
+    public User(){
+        mId = UUID.randomUUID();
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getUserName() {
+        return mUserName;
+    }
+
+    public void setUserName(String userName) {
+        mUserName = userName;
+    }
+
+    public String getPassword() {
+        return mPassword;
+    }
+
+    public void setPassword(String password) {
+        mPassword = password;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+
+}
