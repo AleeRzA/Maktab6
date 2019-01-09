b6cc5e139558ee69929c3cc73dcd18ceb81ae66b
alirezanasr <alireza_nasrollahi@yahoo.com>
2019-01-09 16:38:05 +0330

    UserCursorWrapper updated

--- app/src/main/java/com/example/android/maktab6/database/UserCursorWrapper.java
+++ app/src/main/java/com/example/android/maktab6/database/UserCursorWrapper.java
@@ -5,12 +5,24 @@ import android.database.CursorWrapper;
 
 import com.example.android.maktab6.model.User;
 
+import java.util.UUID;
+
 public class UserCursorWrapper extends CursorWrapper {
     public UserCursorWrapper(Cursor cursor) {
         super(cursor);
     }
 
     public User getUser(){
-        return null;
+        UUID uuid = UUID.fromString(getString(getColumnIndex(DBSchema.UserTable.UserColumns.UUID)));
+        String name = getString(getColumnIndex(DBSchema.UserTable.UserColumns.NAME));
+        String email = getString(getColumnIndex(DBSchema.UserTable.UserColumns.EMAIL));
+        String password = getString(getColumnIndex(DBSchema.UserTable.UserColumns.PASSWORD));
+
+        User user = new User(uuid);
+        user.setName(name);
+        user.setUserName(email);
+        user.setPassword(password);
+
+        return user;
     }
 }
